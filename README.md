9f60c26936c0f320b38dc128a54b2989a29ff240
Ligang Wang <ligangwangs@gmail.com>
2018-03-31 19:42:18 -0400

    add firebase google authentication

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-react-app-starter project demonstrates web application development using react components. <br>
+react-app-starter project demonstrates web application development using react components. [Demo Online](https://react-web-app-starter.firebaseapp.com/)<br>
 
 ## Features
 
 - [Create React App](https://github.com/facebookincubator/create-react-app)
+- [firebaseui-web-react](https://github.com/firebase/firebaseui-web-react)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+
+const firebaseConfig = require('./firebase-config.json');
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
+  uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: ()=>false,
+    },
+  };
+
+  state = {
+    isSignedIn : undefined,
+  };
+
+  componentDidMount(){
+    try{
+      this.unregisterAuthObserver = firebaseApp.auth().onAuthStateChanged((user)=>{
+          this.setState({isSignedIn : !!user});
+      });
+    }catch(error){
+      this.unregisterAuthObserver = null;
+    }
+  }
+
+  componentWillUnmount(){
+    if (this.unregisterAuthObserver)
+      this.unregisterAuthObserver();
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +46,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React App Starter</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.isSignedIn !== undefined && !this.state.isSignedIn &&
+          <div>
+            <StyledFirebaseAuth className="App-intro" uiConfig={this.uiConfig}
+                                firebaseAuth={firebaseApp.auth()}/>
+          </div>
+        }
+        {this.state.isSignedIn &&
+          <div className="App-intro">
+            <a className="Button" onClick={() => firebaseApp.auth().signOut()}>Sign-out</a>
+            <br/><br/>Hello {firebaseApp.auth().currentUser.displayName}
+          </div>
+        }
+
       </div>
     );
   }
